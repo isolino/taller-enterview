b47ecbbfb49cd9d959b683f223fa661add95a4b2
Isolino Ferreira <isolino.ferreira@petzpartner.com.br>
2025-03-17 13:33:03 -0300

    Error and success handle

--- app/src/main/java/com/example/tallerinterview/FirstFragment.kt
+++ app/src/main/java/com/example/tallerinterview/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -44,7 +45,15 @@ class FirstFragment : Fragment() {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.loginState.collect { isProgressing ->
                     binding.progressCircular.isVisible = isProgressing
+                }
+            }
 
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.events.collect { event ->
+                    when (event) {
+                        is LoginViewModel.LoginEvents.Error -> Toast.makeText(requireContext(), event.message, Toast.LENGTH_SHORT).show()
+                        is LoginViewModel.LoginEvents.Success -> Toast.makeText(requireContext(), event.message, Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
 
--- app/src/main/java/com/example/tallerinterview/LoginViewModel.kt
+++ app/src/main/java/com/example/tallerinterview/LoginViewModel.kt
@@ -2,9 +2,12 @@ package com.example.tallerinterview
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
@@ -13,15 +16,31 @@ class LoginViewModel : ViewModel() {
     private val _loginState: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val loginState: StateFlow<Boolean> = _loginState
 
+    private val _events: Channel<LoginEvents> = Channel()
+    val events = _events.receiveAsFlow()
+
 
     fun login(user: String, password: String) {
+
+        val showError = listOf(true, false).random()
 
         viewModelScope.launch {
             _loginState.emit(true)
             delay(5_000)
             _loginState.emit(false)
+
+            if (showError) {
+                _events.send(LoginEvents.Error("ERROR: Wrong Password"))
+            } else {
+                _events.send(LoginEvents.Success("Success Login for $user"))
+            }
         }
 
+    }
+
+    sealed class  LoginEvents{
+        data class Success( val message: String): LoginEvents()
+        data class Error( val message: String): LoginEvents()
     }
 
 }
